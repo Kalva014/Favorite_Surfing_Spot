054807af1464ee29b9999e48a9252fa4b3e64dbd
Kalva014 <kalva014@ucr.edu>
2021-04-03 14:11:19 -0700

    Working on fetching from surf api

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Pretty much on start up
 window.addEventListener("load", () => {
-    const api = "http://magicseaweed.com/api/289/forecast/?spot_id=10";
+    //const apiURL = "http://magicseaweed.com/api/289/forecast/?spot_id=10";
+    const apiURL = "https://services.surfline.com/kbyg/spots/forecasts/{wave}?{params}";
     let minWaveSize;
     let maxWaveSize;
+
+    fetch(apiURL, { 
+        headers: {
+            // remove all those random headers you added
+            "api-key": 000
+        },
+        mode: 'cors' // add this 
+    }).then(response => response.json())
+    .then(function (myJson) {
+        console.log(JSON.stringify(myJson));
+    })
+    .catch(error => console.error(error));
 })
