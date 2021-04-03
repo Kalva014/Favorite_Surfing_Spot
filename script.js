e1c20cc8eeffbb8d71b7efd93194da021049c267
Kalva014 <kalva014@ucr.edu>
2021-04-03 00:54:36 -0700

    Initializing repo

---
+++ script.js
@@ -0,0 +1,6 @@
+// Pretty much on start up
+window.addEventListener("load", () => {
+    const api = "http://magicseaweed.com/api/289/forecast/?spot_id=10";
+    let minWaveSize;
+    let maxWaveSize;
+})
