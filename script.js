4829887b801089a2b8aa0047a61e26240925a3c3
Kalva014 <kalva014@ucr.edu>
2021-04-05 20:30:09 -0700

    Can now fetch data from JSON file using the surfline API

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// Pretty much on start up
+// This function displays the data onto the website
+function displayData(data) {
+    let surfLocation = document.getElementById("location");
+    let minWaveSize = document.getElementById("surf-location-min-wave-size");
+    let maxWaveSize = document.getElementById("surf-location-max-wave-size");
+
+    console.log(data);
+    surfLocation.innerHTML = data.associated.tideLocation.name;
+    minWaveSize.innerHTML = data.data.tides[0].height + data.associated.units.tideHeight;
+    maxWaveSize.innerHTML = data.data.tides[167].height + data.associated.units.tideHeight;
+}
+
+// Pretty much on start up, we fetch the data using the Surfline API
 window.addEventListener("load", () => {
     //const apiURL = "http://magicseaweed.com/api/289/forecast/?spot_id=10";
-    const apiURL = "https://services.surfline.com/kbyg/spots/forecasts/{wave}?{params}";
-    let minWaveSize;
-    let maxWaveSize;
+    const apiURL = "https://services.surfline.com/kbyg/spots/forecasts/tides?spotId=5842041f4e65fad6a770882e&days=6";
 
-    fetch(apiURL, { 
-        headers: {
-            // remove all those random headers you added
-            "api-key": 000
-        },
-        mode: 'cors' // add this 
-    }).then(response => response.json())
-    .then(function (myJson) {
-        console.log(JSON.stringify(myJson));
-    })
-    .catch(error => console.error(error));
+    fetch(apiURL)
+    .then(response => {return response.json()})
+    .then(data => displayData(data));
 })
